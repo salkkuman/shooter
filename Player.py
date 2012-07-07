4fafe4e667302e83f72972b21cfb5eccd6cfaa8b
salkkuman <mikko.salama@gmail.com>
2012-07-08 05:57:22 +0900

    mainista pelaajaa koskevat jutut pelaajaan

--- Player.py
+++ Player.py
@@ -10,7 +10,10 @@ class Player:
         self.speedY = 0
         self.accX = 0
         self.accY = 0
+        self.mov_speed = 3
     
+    def movespeed(self,movespeed):
+        self.mov_speed = movespeed
     def reset(self):
         self.x = 200
         self.y = 200
@@ -22,88 +25,34 @@ class Player:
         self.x+=x
         self.y+=y
         
-    def changespeed(self,x,y):    
-        self.speedX+=x
-        self.speedY+=y
-        
-    def changeacc(self,x,y):     
-        self.accX+=x
-        self.accY+=y
-        if(self.accX>0.42):
-            self.accX=0.42
-        if(self.accY>0.42):
-            self.accY=0.42
-        
-        if(self.accX<-0.42):
-            self.accX=-0.42
-        if(self.accY<-0.42):
-            self.accY=-0.42
-        
-        if(self.accX<0.1 and self.accX>0):
-            self.accX=0.1
-        if(self.accX>-0.1 and self.accX<0):
-            self.accX=-0.1
-            
-        if(self.accY<0.1 and self.accY>0):
-            self.accY=0.1
-        if(self.accY>-0.1 and self.accY<0):
-            self.accY=-0.1
-    
-    def stop(self):
-        self.speedX = 0
-        self.speedY = 0
-        self.accX = 0
-        self.accY = 0
-        
-    def stopX(self):
-        self.speedX = 0
+    def changespeedX(self,x):    
+        self.speedX=x
+    def changespeedY(self,y):    
+        self.speedY=y
         
         
-        
-    def stopY(self):
-        
-        self.speedY = 0
-        
-        
-   
+
     
     def move(self):
-        self.x+=int(self.speedX)
+        x = self.speedX * self.mov_speed
+        y = self.speedY * self.mov_speed
+        self.x+=x
         if(self.x<40):
             self.x=40
             
         if(self.x>720):
             self.x=720
             
-        self.y+=int(self.speedY)
+        self.y+=y
         if(self.y<0):
             self.y=0
             
         if(self.y>720):
             self.y=720
            
-        self.speedX+=self.accX
-        self.speedY+=self.accY
         
-        minspeed=0.1
-        if(self.speedY<minspeed and self.speedY>0):
-            self.speedY=minspeed
-        if(self.speedX<minspeed and self.speedX>0):
-            self.speedX=-minspeed
-        if(self.speedY>minspeed and self.speedY<0):
-            self.speedY=minspeed
-        if(self.speedX>minspeed and self.speedX<0):
-            self.speedX=-minspeed
         
-        maxspeed=4.2
-        if(self.speedY>maxspeed):
-            self.speedY=maxspeed
-        if(self.speedX>maxspeed):
-            self.speedX=maxspeed
-        if(self.speedY<-maxspeed):
-            self.speedY=-maxspeed
-        if(self.speedX<-maxspeed):
-            self.speedX=-maxspeed
+
     # hp = ""
     #x = 0
     #y = 0
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 import Player
 import EnemyUnit
 import Bullet
@@ -11,14 +12,14 @@ white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
-yellow = (255,255,0)
+yellow = (255, 255, 0)
 transparent = (255, 0, 255)
 pi = 3.141592653
 done = False
 
 size = [1024, 720]
 screen = pygame.display.set_mode(size)
-screen.set_colorkey((255,0,255))
+screen.set_colorkey((255, 0, 255))
 
 font = pygame.font.Font(None, 21)
 
@@ -28,55 +29,110 @@ background_tile = pygame.image.load("tile.png").convert()
 
 player = Player.Player()
 
-# This sets the width and height of each grid location in backround
-marginX=40
-marginY=40
-width=40
-height=40
+# This sets the width and height of each grid location in background
+marginX = 40
+marginY = 40
+width = 40
+height = 40
 
 hit = 0
-bestscore=0
+bestscore = 0
 pygame.display.set_caption("Herpderpsburdoborde:DDDDDD:D")
 iteration = 0
 bulletlist = []
 enemylist = []
 ammolist = []
 score = 0
+keystate_left = False
+keystate_right = False
+keystate_up = False
+keystate_down = False
+speedx = 0
+speedy = 0
+mov_speed = 3 #default movement speed
+
 while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     
-    # Set the speed based on the key pressed
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.changeacc(-0.3,0)
-            if event.key == pygame.K_RIGHT:
-                player.changeacc(0.3,0)
-            if event.key == pygame.K_UP:
-                player.changeacc(0,-0.3)
-            if event.key == pygame.K_DOWN:
-                player.changeacc(0,0.3)
-                 
-        # Reset speed when key goes up      
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player.changeacc(0.3,0)
-                player.stopX()
-                
-            if event.key == pygame.K_RIGHT:
-                player.changeacc(-0.3,0)
-                player.stopX()
-                
-            if event.key == pygame.K_UP:
-                player.changeacc(0,0.3)
-                player.stopY()
-                
-            if event.key == pygame.K_DOWN:
-                player.changeacc(0,-0.3)
-                player.stopY()
-                
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_1:
+            player.movespeed(1)
+        if event.key == pygame.K_2:
+            player.movespeed(2)
+        if event.key == pygame.K_3:
+            player.movespeed(3)
+        if event.key == pygame.K_4:
+            player.movespeed(4)
+        if event.key == pygame.K_5:
+            player.movespeed(5)
+        if event.key == pygame.K_6:
+            player.movespeed(6)
+        if event.key == pygame.K_7:
+            player.movespeed(7)
+        if event.key == pygame.K_8:
+            player.movespeed(8)
+        if event.key == pygame.K_9:
+            player.movespeed(9)
+    #uus inputkoodi alku
+    key = pygame.key.get_pressed()
     
+    if keystate_left == False:
+        if key[pygame.K_LEFT]:
+            keystate_left = True
+            player.changespeedX(-1)
+    if keystate_right == False:
+        if key[pygame.K_RIGHT]:
+            keystate_right = True
+            player.changespeedX(1)
+    if keystate_up == False:
+        if key[pygame.K_UP]:
+            keystate_up = True
+            player.changespeedY(-1)
+    if keystate_down == False:
+        if key[pygame.K_DOWN]:
+            keystate_down = True
+            player.changespeedY(1)
+
+        
+        
+    if keystate_left == True:
+        if key[pygame.K_LEFT] == False:
+            keystate_left = False
+            if key[pygame.K_RIGHT] == False:
+                player.changespeedX(0)
+            else:
+                player.changespeedX(1)
+                
+    if keystate_right == True:
+        if key[pygame.K_RIGHT] == False:
+            keystate_right = False
+            if key[pygame.K_LEFT] == False:
+                player.changespeedX(0)
+            else:
+                player.changespeedX(-1)
+            
+    if keystate_up == True:
+        if key[pygame.K_UP] == False:
+            keystate_up = False         
+            if key[pygame.K_DOWN] == False:
+                player.changespeedY(0)
+            else:
+                player.changespeedY(1)
+                
+                
+    if keystate_down == True:
+        if key[pygame.K_DOWN] == False:
+            keystate_down = False
+            if key[pygame.K_UP] == False:
+                player.changespeedY(0)
+            else:
+                player.changespeedY(-1)
+                
+    #uus inputcode loppu
+    
+               
     screen.fill(black)
     
     pygame.draw.line(screen, transparent, [768, 0], [768, 767], 10) #sivupalkki
@@ -87,14 +143,15 @@ while done == False:
     #moving backround 
     for x in range(17):
         for y in range(19):
-            screen.blit(background_tile, [x*width+marginX,(height)*y-marginY])
+            screen.blit(background_tile, [x * width + marginX, (height) * y - marginY])
     
-    marginY-=3
-    if marginY<=0:
-        marginY=40
+    marginY -= 3
+    if marginY <= 0:
+        marginY = 40
     
         
     #draw player and move player
+    
     player.move()
     pygame.draw.circle(screen, red, (player.x, player.y), 10, 0)
     #ammo
@@ -107,27 +164,27 @@ while done == False:
             ammolist.remove(ammo)
     
     #add bullets and enemy units
-    if(iteration%15==0):
-        ammolist.append(Ammo.Ammo(player.x,player.y))
+    if(iteration % 15 == 0):
+        ammolist.append(Ammo.Ammo(player.x, player.y))
         
-    if(iteration%50==0):
+    if(iteration % 50 == 0):
         
-        enemylist.append(EnemyUnit.EnemyUnit(random.randint(40, 720),0,1))
+        enemylist.append(EnemyUnit.EnemyUnit(random.randint(40, 720), 0, 1))
    
         #bulletlist.append(Bullet.Bullet(40,40,0,5, [[10,20],[10,20]]))
     
     for enemy in enemylist:
         enemy.shoot(bulletlist)
         enemy.time()
-        if(player.x>enemy.x+enemy.hitbox[0][0] and player.x<enemy.x+enemy.hitbox[0][1] and player.y>enemy.y+enemy.hitbox[1][0] and player.y<enemy.y+enemy.hitbox[1][1]):
+        if(player.x > enemy.x + enemy.hitbox[0][0] and player.x < enemy.x + enemy.hitbox[0][1] and player.y > enemy.y + enemy.hitbox[1][0] and player.y < enemy.y + enemy.hitbox[1][1]):
             hit = 1
         
-        if (hit==1):    
+        if (hit == 1):    
             bulletlist = []
             enemylist = []
             player.reset()
             iteration = 0
-            hit=0
+            hit = 0
             
     #draw and move enemy unit and bullets
     for enemy in enemylist:
@@ -137,23 +194,23 @@ while done == False:
     
     for ammo in ammolist:
         for enemy in enemylist:
-            if(ammo.x>enemy.x+enemy.hitbox[0][0] and ammo.x<enemy.x+enemy.hitbox[0][1] and ammo.y>enemy.y+enemy.hitbox[1][0] and ammo.y<enemy.y+enemy.hitbox[1][1]):
+            if(ammo.x > enemy.x + enemy.hitbox[0][0] and ammo.x < enemy.x + enemy.hitbox[0][1] and ammo.y > enemy.y + enemy.hitbox[1][0] and ammo.y < enemy.y + enemy.hitbox[1][1]):
                 enemylist.remove(enemy)
-                score+=100
+                score += 100
             else:
                 enemy.draw(screen)
     for bullet in bulletlist:
         
-        if(player.x>bullet.x+bullet.hitbox[0][0] and player.x<bullet.x+bullet.hitbox[0][1] and player.y>bullet.y+bullet.hitbox[1][0] and player.y<bullet.y+bullet.hitbox[1][1]):
+        if(player.x > bullet.x + bullet.hitbox[0][0] and player.x < bullet.x + bullet.hitbox[0][1] and player.y > bullet.y + bullet.hitbox[1][0] and player.y < bullet.y + bullet.hitbox[1][1]):
             hit = 1
         
-        if (hit==1):    
+        if (hit == 1):    
             bulletlist = []
             enemylist = []
             player.reset()
             iteration = 0
-            score=0
-            hit=0
+            score = 0
+            hit = 0
             
     #draw and move enemy unit and bullets
     for bullet in bulletlist:
@@ -165,16 +222,16 @@ while done == False:
     
     for enemy in enemylist:
         
-        if(player.x>enemy.x+enemy.hitbox[0][0] and player.x<enemy.x+enemy.hitbox[0][1] and player.y>enemy.y+enemy.hitbox[1][0] and player.y<enemy.y+enemy.hitbox[1][1]):
+        if(player.x > enemy.x + enemy.hitbox[0][0] and player.x < enemy.x + enemy.hitbox[0][1] and player.y > enemy.y + enemy.hitbox[1][0] and player.y < enemy.y + enemy.hitbox[1][1]):
             hit = 1
         
-        if (hit==1):    
+        if (hit == 1):    
             bulletlist = []
             enemylist = []
             ammolist = []
             player.reset()
             iteration = 0
-            hit=0
+            hit = 0
             
     #draw and move enemy unit and bullets
     for enemy in enemylist:
@@ -184,11 +241,11 @@ while done == False:
             enemy.draw(screen)
     # Render the text for score
     
-    if(score>bestscore):
-        bestscore=score
-    text = font.render('Best Score'+str(bestscore), True, (white), (black))
+    if(score > bestscore):
+        bestscore = score
+    text = font.render('Best Score' + str(bestscore), True, (white), (black))
     
-    text1 = font.render('Current Score'+str(score), True, (white), (black))
+    text1 = font.render('Current Score' + str(score), True, (white), (black))
     # Create a rectangle
     textRect = text.get_rect()
     textRect1 = text1.get_rect()
@@ -202,7 +259,7 @@ while done == False:
     screen.blit(text1, textRect1)
 
 
-    score +=1
+    score += 1
     iteration += 1
     pygame.display.flip()    
     clock.tick(60)
